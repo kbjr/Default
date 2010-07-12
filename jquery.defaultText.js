52db479283728f81ba736d60decaa460764157b9
James Brumond <kbjr14@gmail.com>
2010-07-12 00:06:00 -0700

    added functions, v0.3.1

--- jquery.defaultText.js
+++ jquery.defaultText.js
@@ -2,9 +2,9 @@
  * jquery.defaultText.js
  * 
  * Author: James Brumond
- * Version: 0.2.4
+ * Version: 0.3.1
  * Date Created: 2 June 2010
- * Date Last Mod: 8 July 2010
+ * Date Last Mod: 12 July 2010
  *
  * Copyright 2010 James Brumond
  * Dual licensed under MIT and GPL
@@ -61,69 +61,117 @@ getStyle = function(elem, styleProp) {
 
 $.fn.defaultText = function(options) {
 	var options = options || { };
-	options.defText = options.defText || '';
-	options.textStyle = options.textStyle || { };
-	options.inputStyle = options.inputStyle || { };
-	options.onfocus = options.onfocus || function() { };
-	options.onblur = options.onblur || function() { };
+	if (typeof options != 'string') {
+		options.defText = options.defText || '';
+		options.textStyle = options.textStyle || { };
+		options.inputStyle = options.inputStyle || { };
+		options.onfocus = options.onfocus || function() { };
+		options.onblur = options.onblur || function() { };
+	}
 	return this.filter('input[type=text], input[type=password], textarea').each(function() {
-		// build the text element and put it in the document
-		var input = this,
-		text = (options.defText == '') ? input.title : options.defText,
-		span = input.parentNode.appendChild(_elem('span', {
-			style: {
-				position: 'absolute',
-				padding: '0px',
-				margin: '0px',
-				fontFamily: getStyle(input, 'fontFamily'),
-				fontSize: getStyle(input, 'fontSize'),
-				fontWeight: getStyle(input, 'fontWeight'),
-				color: getStyle(input, 'color'),
-				width: getStyle(input, 'width'),
-				cursor: 'text',
-				backgroundColor: getStyle(input, 'backgroundColor'),
-				// contribution by whatcould <http://github.com/whatcould>
-				zIndex: (parseInt(getStyle(this, 'zIndex')) || 0) + 1
-			},
-			innerHTML: text,
-			className: 'default'
-		})),
-		// define the click/focus event function
-		click = function() {
-			$(span).hide();
-			input.focus();
-			options.onfocus.call(input);
-		},
-		position = function() {
-			span.style.top = parseInt(getStyle(input, 'paddingTop')) +
-				parseInt(getStyle(input, 'borderTopWidth')) + input.offsetTop + 'px';
-			span.style.left = parseInt(getStyle(input, 'paddingLeft')) +
-				parseInt(getStyle(input, 'borderLeftWidth')) + input.offsetLeft + 'px';
-		};
-		if (String(input.id).length > 0) {
-			span.id = 'default-' + input.id;
-		}
-		setStyle(span, options.textStyle);
-		setStyle(input, options.inputStyle);
-		// change/add input properties
-		if (input.defValue) input.parentNode.removeChild(input.defValue);
-		input.defValue = span;
-		// add the click/focus event
-		$(input).add(span).focus(function() { click(); }).click(function() { click(); });
-		$(input).blur(function() {
-			if (input.value == '') {
-				$(span).show();
+		var input = this;
+		if (typeof input._defaultText == 'undefined') {
+			if (typeof options == 'string') return true;
+			input._defaultText = {
+				options: options,
+				reset: function() {
+					setStyle(input._defaultText.span, {
+						position: 'absolute',
+						padding: '0px',
+						margin: '0px',
+						fontFamily: getStyle(input, 'fontFamily'),
+						fontSize: getStyle(input, 'fontSize'),
+						fontWeight: getStyle(input, 'fontWeight'),
+						color: getStyle(input, 'color'),
+						width: getStyle(input, 'width'),
+						cursor: 'text',
+						backgroundColor: getStyle(input, 'backgroundColor'),
+						// contribution by whatcould <http://github.com/whatcould>
+						zIndex: (parseInt(getStyle(input, 'zIndex')) || 0) + 1
+					});
+					setStyle(span, input._defaultText.options.textStyle);
+					setStyle(input, input._defaultText.options.inputStyle);
+					input._defaultText.position();
+				},
+				span: null,
+				click: null,
+				position: null
 			}
-			options.onblur.call(input);
-		});
-		// add a window.resize event to make sure the positioning doesn't change
-		$(window).resize(position);
-		$(window).focus(position);
-		// FIXME sometimes it doesn't quite take, so call
-		// it a couple times
-		window.setTimeout(position, 50);
-		window.setTimeout(position, 500);
-		window.setTimeout(position, 3000);
+		}
+		if (typeof options == 'string') {
+			switch (options) {
+				case 'reset':
+					if (typeof input._defaultText.reset != 'function') return true;
+					input._defaultText.reset();
+					break;
+				case 'destroy':
+					if (typeof input._defaultText.reset != 'function') return true;
+					input._defaultText.span.parentNode.removeChild(input._defaultText.span);
+					input._defaultText = false;
+					break;
+				default: break;
+			}
+		} else {
+			// build the text element and put it in the document
+			var text = (options.defText == '') ? input.title : options.defText,
+			span = input.parentNode.appendChild(_elem('span', {
+				style: {
+					position: 'absolute',
+					padding: '0px',
+					margin: '0px',
+					fontFamily: getStyle(input, 'fontFamily'),
+					fontSize: getStyle(input, 'fontSize'),
+					fontWeight: getStyle(input, 'fontWeight'),
+					color: getStyle(input, 'color'),
+					width: getStyle(input, 'width'),
+					cursor: 'text',
+					backgroundColor: getStyle(input, 'backgroundColor'),
+					// contribution by whatcould <http://github.com/whatcould>
+					zIndex: (parseInt(getStyle(input, 'zIndex')) || 0) + 1
+				},
+				innerHTML: text,
+				className: 'default'
+			}));
+			// define the click/focus event function
+			input._defaultText.click = function() {
+				$(span).hide();
+				input.focus();
+				options.onfocus.call(input);
+			};
+			input._defaultText.position = function() {
+				span.style.top = parseInt(getStyle(input, 'paddingTop')) +
+					parseInt(getStyle(input, 'borderTopWidth')) + input.offsetTop + 'px';
+				span.style.left = parseInt(getStyle(input, 'paddingLeft')) +
+					parseInt(getStyle(input, 'borderLeftWidth')) + input.offsetLeft + 'px';
+			};
+			if (String(input.id).length > 0) {
+				span.id = 'default-' + input.id;
+			}
+			setStyle(span, options.textStyle);
+			setStyle(input, options.inputStyle);
+			// change/add input properties
+			if (input.defValue) input.parentNode.removeChild(input.defValue);
+			input.defValue = span;
+			// add the click/focus event
+			$(input).add(span).focus(function() {
+				input._defaultText.click(); }).click(function() {
+				input._defaultText.click();
+			});
+			$(input).blur(function() {
+				if (input.value == '') $(span).show();
+				options.onblur.call(input);
+			});
+			// add a window.resize event to make sure the positioning doesn't change
+			$(window).resize(input._defaultText.position);
+			$(window).focus(input._defaultText.position);
+			// FIXME sometimes it doesn't quite take, so call
+			// it a couple times
+			window.setTimeout(input._defaultText.position, 50);
+			window.setTimeout(input._defaultText.position, 500);
+			window.setTimeout(input._defaultText.position, 3000);
+			// add the span to the element's _defaultText property
+			input._defaultText.span = span;
+		}
 	});
 };
 
